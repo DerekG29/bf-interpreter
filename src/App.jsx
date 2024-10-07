d7e8272bd1c6456026d068a4fa22d2677ffa473c
DerekG29 <derekgrubbs29@gmail.com>
2024-10-07 19:36:14 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>Hello World!</div>
+      
+      <div className="input-container"></div>
+
+      <div className="output-container"></div>
+    </>
+  )
+}
+
+export default App
